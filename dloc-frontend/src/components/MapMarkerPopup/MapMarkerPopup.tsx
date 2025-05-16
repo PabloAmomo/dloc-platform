65250a30b709c7de1200aacd11bc86377516a196
Pablo Amommo <pablo.amomo@gmail.com>
2025-05-16 18:45:56 +0200

    Add Bolt icon to MapMarkerPopup for changeToFull action

--- dloc-frontend/src/components/MapMarkerPopup/MapMarkerPopup.tsx
+++ dloc-frontend/src/components/MapMarkerPopup/MapMarkerPopup.tsx
@@ -5,7 +5,7 @@ import { MapPath } from "models/MapPath";
 import { useDevicesFormContext } from "providers/DeviceFormProvider";
 import { useMapContext } from "providers/MapProvider";
 import { useTranslation } from "react-i18next";
-import { PowerTwoTone, Wifi, WifiOff } from "@mui/icons-material";
+import { Bolt, PowerTwoTone, Wifi, WifiOff } from "@mui/icons-material";
 import BatteryIcon from "components/BatteryIcon/BatteryIcon";
 import calculateTime from "functions/calculateTime";
 import convertUTCDateToLocalDate from "functions/convertUTCDateToLocalDate";
@@ -183,7 +183,9 @@ const MapMarkerPopup = (props: MapMarkerPopupProps) => {
             onClick={handleChangeToAutomaticFull}
             {...style.ChangeToAutomaticFullProps}
           >
+            <Bolt {...style.ItemIconProp} />
             {t("changeToFull")}
+            <Bolt {...style.ItemIconProp} />
           </Box>
         )}
 
