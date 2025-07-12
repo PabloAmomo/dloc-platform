f531cb4d276a5e726019dc53db0a603008a84024
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-12 12:44:10 +0200

    Refactor jt808ParseTerminalAttributes: remove unused terminalType parsing logic and adjust offset initialization

--- dloc-server/source/services/server-socket/protocol808/functions/jt808ParseTerminalAttributesBits.ts
+++ dloc-server/source/services/server-socket/protocol808/functions/jt808ParseTerminalAttributesBits.ts
@@ -3,18 +3,7 @@ import { Jt808TerminalAttributes } from "../models/Jt808TerminalAttributes";
 const jt808ParseTerminalAttributes = (
   body: Buffer
 ): Jt808TerminalAttributes => {
-  let offset = 0;
-
-  const terminalTypeRaw = body.readUInt16BE(offset);
-  offset += 2;
-  //const terminalType = {
-  //  passengerVehicle: !!(terminalTypeRaw & (1 << 0)),
-  //  dangerousGoodsVehicle: !!(terminalTypeRaw & (1 << 1)),
-  //  ordinaryFreightVehicle: !!(terminalTypeRaw & (1 << 2)),
-  //  rentalCar: !!(terminalTypeRaw & (1 << 3)),
-  //  hardDiskVideo: !!(terminalTypeRaw & (1 << 6)),
-  //  splitMachine: !!(terminalTypeRaw & (1 << 7)),
-  //};
+  let offset = 2;
 
   const manufacturerId = body.subarray(offset, offset + 5).toString("ascii");
   offset += 5;
