59996baa4990b6ec06a91ccf25ce7ce1d0e88ea5
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-12 21:09:32 +0200

    Refactor jt808CreateTemporaryLocationTrackingPacket: simplify parameter handling and logging

--- dloc-server/source/services/server-socket/protocol808/functions/jt808CreateTemporaryLocationTrackingPacket.ts
+++ dloc-server/source/services/server-socket/protocol808/functions/jt808CreateTemporaryLocationTrackingPacket.ts
@@ -3,18 +3,14 @@ import jt808CreateFrameData from "./jt808CreateFrameData";
 // TODO: No funciona y no se porque
 const jt808CreateTemporaryLocationTrackingPacket = (terminalId : string, counter : number ) : Buffer => {
 
-  const parametersCount = "03"; 
-  const paramter1 = "002E 00FF"; 
+  const paramter1 = "00 05 00 00 00 3C".replace(/ /g, ""); 
 
-  let paramList = parametersCount + paramter1;
-
-  paramList = paramList.replace(/ /g, ""); // Remove spaces
-  console.log(`-----------> check Location Tracking Controls: ${paramList}`);
+  console.log(`-----------> check Location Tracking Controls: ${paramter1}`);
   const packet =  jt808CreateFrameData({
     msgType: 0x8202,
     terminalId: Buffer.from(terminalId, "hex"),
     msgSerialNumber: counter,
-    body: Buffer.from(paramList, "hex"),
+    body: Buffer.from(paramter1, "hex"),
   });
 
   return packet;
