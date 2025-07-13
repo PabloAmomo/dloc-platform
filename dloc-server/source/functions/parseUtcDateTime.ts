aad642190e4a0b87ae169ae1538d3f3c01a20115
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-13 20:29:39 +0200

    Refactor parseUtcDateTime function for consistency in string comparison and formatting

--- dloc-server/source/functions/parseUtcDateTime.ts
+++ dloc-server/source/functions/parseUtcDateTime.ts
@@ -1,15 +1,20 @@
 import { isValidDate } from "./isValidDate";
 
 const parseUtcDateTime = (date: string, time: string): Date | null => {
-  if (date == '') return null;
-  if (time == '') time = '000000';
+  if (date === "") return null;
+  if (time === "") time = "000000";
 
-  const dateValue: Date = new Date(`
-  20${date.substring(0, 2)}-${date.substring(2, 4)}-${date.substring(4, 6)}T
-  ${time.substring(0, 2)}:${time.substring(2, 4)}:${time.substring(4, 6)}.000Z
-  `);
+  const dateValue: Date = new Date(
+    `20${date.substring(0, 2)}-${date.substring(2, 4)}-${date.substring(
+      4,
+      6
+    )}T${time.substring(0, 2)}:${time.substring(2, 4)}:${time.substring(
+      4,
+      6
+    )}.000Z`
+  );
 
   return isValidDate(dateValue) ? dateValue : null;
 };
 
-export { parseUtcDateTime }
+export { parseUtcDateTime };
