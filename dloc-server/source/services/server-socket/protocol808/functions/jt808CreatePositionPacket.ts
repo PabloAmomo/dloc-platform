5495f4eaf8666a6d2f1025f03407391142bf4a05
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-13 11:36:28 +0200

    Refactor jt808CreatePositionPacket: improve time validation logic for location packets

--- dloc-server/source/services/server-socket/protocol808/functions/jt808CreatePositionPacket.ts
+++ dloc-server/source/services/server-socket/protocol808/functions/jt808CreatePositionPacket.ts
@@ -1,15 +1,35 @@
-import { GpsAccuracy } from '../../../../models/GpsAccuracy';
-import { PositionPacket } from '../../../../models/PositionPacket';
-import { printMessage } from '../../../../functions/printMessage';
-import { Jt808LocationPacket } from '../models/Jt808LocationPacket';
-import parseDateTimeToUtcDateTime from '../../../../functions/parseDateTimeToUtcDateTime';
+import { GpsAccuracy } from "../../../../models/GpsAccuracy";
+import { PositionPacket } from "../../../../models/PositionPacket";
+import { printMessage } from "../../../../functions/printMessage";
+import { Jt808LocationPacket } from "../models/Jt808LocationPacket";
 
-const jt808CreatePositionPacket = (imei: string, remoteAddress: string, locationPacket: Jt808LocationPacket, activity: string): PositionPacket | undefined => {
+const MAX_TIME_DIFFERENCE_MS = 300000; // 5 minutes in milliseconds
+
+const jt808CreatePositionPacket = (
+  imei: string,
+  remoteAddress: string,
+  locationPacket: Jt808LocationPacket,
+  activity: string
+): PositionPacket | undefined => {
   try {
+    const dateTimeUtc = new Date(locationPacket.dateTimeUTC);
+
+    const currentTimeUtc = new Date();
+    const timeDifference = Math.abs(
+      currentTimeUtc.getTime() - dateTimeUtc.getTime()
+    );
+
+    if (timeDifference > MAX_TIME_DIFFERENCE_MS) {
+      printMessage(
+        `[${imei}] (${remoteAddress}) ❌ Location packet date/time is not within 5 minutes of the current time.`
+      );
+      return undefined;
+    }
+
     return {
       imei,
       remoteAddress,
-      dateTimeUtc: new Date(locationPacket.dateTimeUTC),
+      dateTimeUtc,
       valid: locationPacket.lat !== 0 && locationPacket.lng !== 0,
       lat: locationPacket.lat,
       lng: locationPacket.lng,
@@ -17,11 +37,13 @@ const jt808CreatePositionPacket = (imei: string, remoteAddress: string, location
       directionAngle: locationPacket.direction,
       gsmSignal: locationPacket.gsmSignal ?? -1,
       batteryLevel: locationPacket.batteryLevel ?? -1,
-      accuracy: GpsAccuracy.gps, 
+      accuracy: GpsAccuracy.gps,
       activity,
     };
   } catch (err: Error | any) {
-    printMessage(`[${imei}] (${remoteAddress}) ❌ error creating position packet [${err.message}]`);
+    printMessage(
+      `[${imei}] (${remoteAddress}) ❌ error creating position packet [${err.message}]`
+    );
     return undefined;
   }
 };
