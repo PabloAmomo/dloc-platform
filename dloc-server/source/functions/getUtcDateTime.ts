91d6f69305d4c8d9a04f36de2e4f144e85afdb75
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-13 09:02:59 +0200

    Refactor getUtcDateTime: inline twoDigits function into getUtcDateTime and remove separate twoDigits module

--- dloc-server/source/functions/getUtcDateTime.ts
+++ dloc-server/source/functions/getUtcDateTime.ts
@@ -1,12 +1,25 @@
-import { twoDigits } from "./twoDigits";
-
-const getUtcDateTime = (withSeparators: boolean = true) : string => {
+const getUtcDateTime = (withSeparators: boolean = true): string => {
   const date = new Date();
-  const sepDate = (withSeparators ? "-" : "");
-  const sepTime = (withSeparators ? ":" : "");
-  return "" + date.getUTCFullYear() + sepDate + twoDigits(date.getUTCMonth() + 1) + sepDate + twoDigits(date.getUTCDate()) +
+  const sepDate = withSeparators ? "-" : "";
+  const sepTime = withSeparators ? ":" : "";
+  return (
+    "" +
+    date.getUTCFullYear() +
+    sepDate +
+    twoDigits(date.getUTCMonth() + 1) +
+    sepDate +
+    twoDigits(date.getUTCDate()) +
     (withSeparators ? " " : "") +
-    twoDigits(date.getUTCHours()) + sepTime + twoDigits(date.getUTCMinutes()) + sepTime + twoDigits(date.getUTCSeconds());
-}
+    twoDigits(date.getUTCHours()) +
+    sepTime +
+    twoDigits(date.getUTCMinutes()) +
+    sepTime +
+    twoDigits(date.getUTCSeconds())
+  );
+};
 
-export { getUtcDateTime }
+const twoDigits = (value: number): string => {
+  return value < 10 ? "0" + value : "" + value;
+};
+
+export { getUtcDateTime };
--- dloc-server/source/functions/twoDigits.ts
+++
@@ -1,5 +0,0 @@
-const twoDigits = (value:number) : string => {
-  return value < 10 ? '0' + value : '' + value;
-}
-
-export { twoDigits }
