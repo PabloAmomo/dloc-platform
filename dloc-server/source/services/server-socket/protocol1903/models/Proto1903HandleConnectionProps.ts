8de00dd6f826d1c826ab64dd4411208fd94df1ef
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-15 21:52:40 +0200

    Refactor protocol handling by removing deprecated jt808Ingress and proto1903Ingress files. Update related interfaces for improved clarity and maintainability.

--- dloc-server/source/services/server-socket/protocol1903/models/Proto1903HandleConnectionProps.ts
+++ dloc-server/source/services/server-socket/protocol1903/models/Proto1903HandleConnectionProps.ts
@@ -1,5 +1,4 @@
 import { Persistence } from "../../../../models/Persistence";
-import net from "node:net";
 import Proto1903HandlePacket from "./Proto1903HandlePacket";
 
 export default interface Proto1903HandleConnectionProps {
@@ -8,6 +7,5 @@ export default interface Proto1903HandleConnectionProps {
   data: String;
   handlePacket: Proto1903HandlePacket;
   persistence: Persistence;
-  conn: net.Socket | { write: (arg0: string) => void; destroy: () => void };
   counter: number;
 }
