9fd7cd4f53bdd59df55f6aa4772dc75c957fd330
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-16 15:52:49 +0200

    Fix import path for Jt808ProcessPacketProps and add interface definition for packet processing properties.

--- dloc-server/source/services/server-socket/protocol808/models/Jt808ProcessPacket.ts
+++ dloc-server/source/services/server-socket/protocol808/models/Jt808ProcessPacket.ts
@@ -1,4 +1,4 @@
-import Jt808ProcessPacketProps from "./Jt808ProcessPacketprops";
+import Jt808ProcessPacketProps from "./Jt808ProcessPacketPropss";
 
 export type Jt808ProcessPacket = (props: Jt808ProcessPacketProps) => {
   updateLastActivity: boolean;
