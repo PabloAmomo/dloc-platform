c2c87ff8387c80ea109edaa05cd5577b2b969a17
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-16 21:05:29 +0200

    Enhance showAlert function to support multiline messages; update powerProfileUpdated strings in English and Spanish for improved clarity.

--- dloc-frontend/src/functions/showAlert.tsx
+++ dloc-frontend/src/functions/showAlert.tsx
@@ -3,8 +3,24 @@ import { Button } from "@mui/material";
 import { Colors } from "enums/Colors";
 import { SnackbarKey, closeSnackbar, enqueueSnackbar } from "notistack";
 
-const showAlert = (message: string, variant: "default" | "error" | "success" | "warning" | "info") => {
-  enqueueSnackbar(message, {
+const showAlert = (
+  message: string,
+  variant: "default" | "error" | "success" | "warning" | "info"
+) => {
+  // TODO: Make the message multiline if it contains \r\n
+  let messageJSX: React.ReactNode | undefined = undefined;
+  if (message.includes("\r\n"))
+    messageJSX = (
+      <>
+        {message.split("\r\n").map((line, idx: number) => (
+          <div key={idx}>
+            {line}
+          </div>
+        ))}
+      </>
+    );
+
+  enqueueSnackbar(messageJSX ? messageJSX : message, {
     variant: variant,
     autoHideDuration: 5000,
     persist: false,
@@ -18,5 +34,3 @@ const showAlert = (message: string, variant: "default" | "error" | "success" | "
 };
 
 export default showAlert;
-
- 
