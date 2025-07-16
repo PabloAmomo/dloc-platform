a8240b890741396ad40eb8607d2492f2a957bf77
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-16 07:16:50 +0200

    Add handleConnection function and HandleConnectionProps interface for improved packet handling

---
+++ dloc-server/source/services/server-socket/functions/handleConnection.ts
@@ -0,0 +1,69 @@
+import convertStringToHexString from "../../../functions/convertStringToHexString";
+import { getNormalizedIMEI } from "../../../functions/getNormalizedIMEI";
+import { printMessage } from "../../../functions/printMessage";
+import HandleConnectionProps from "../models/HandleConnectionProp";
+import HandlePacketResult from "../models/HandlePacketResult";
+
+const handleConnection = async ({
+  imei,
+  remoteAddress,
+  data,
+  handlePacket,
+  persistence,
+  counter,
+  disconnect,
+}: HandleConnectionProps): Promise<HandlePacketResult[]> => {
+  /** results */
+  const results: HandlePacketResult[] = [];
+
+  // TODO: [REFACTOR] Create a common handler for protocols 808 and 1903
+
+  if (data.length === 0) {
+    printMessage(
+      `[${getNormalizedIMEI(
+        imei
+      )}] (${remoteAddress}) ❌ error decoding packet.`
+    );
+    return results;
+  }
+
+  const isBuffer = Buffer.isBuffer(data[0]);
+
+  /** Process each packet */
+  for (let i = 0; i < data.length; i++) {
+    /** Discart empty packets */
+    if (!data[i] || data[i].length === 0) continue;
+
+    if (!isBuffer) data[i] = data[i] + "#";
+
+    /** Handle packet */
+    try {
+      await handlePacket({
+        imei,
+        remoteAddress,
+        data: data[i] as any,
+        persistence,
+        counter,
+        disconnect,
+      }).then((result: HandlePacketResult) => {
+        /** Save result */
+        results.push(result);
+        /** Error handling packet */
+        if (result.error !== "") throw new Error(result.error);
+        /** Update imei */
+        if (result.imei !== "") imei = result.imei;
+      });
+    } catch (err: Error | any) {
+      printMessage(
+        `[${getNormalizedIMEI(imei)}] (${remoteAddress}) ❌ error handling packet (1) (${
+          err?.message ?? "unknown error"
+        }) packet [${isBuffer ? convertStringToHexString(data[i]) : ((data[i] as string)?.split(",")?.[0] ?? data[i])}]`
+      );
+    }
+  }
+
+  /** Return results */
+  return results;
+};
+
+export default handleConnection;
---
+++ dloc-server/source/services/server-socket/models/HandleConnectionProp.ts
@@ -0,0 +1,13 @@
+import { Persistence } from '../../../models/Persistence';
+import Jt808HandlePacket from '../protocol808/models/Jt808HandlePacket';
+import Proto1903HandlePacket from '../protocol1903/models/Proto1903HandlePacket';
+
+export default  interface HandleConnectionProps {
+  imei: string;
+  remoteAddress: string;
+  data: Buffer[] | string[]; 
+  handlePacket: Proto1903HandlePacket | Jt808HandlePacket;
+  persistence: Persistence;
+  counter: number; 
+  disconnect: () => void;
+}
