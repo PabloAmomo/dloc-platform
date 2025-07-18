45940d595f35437bec40c5db83ba6c9bab1edca1
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-18 12:46:17 +0200

    Add parameter setting packet creation for low battery alarm in jt808ProcessPacket0x0102

--- dloc-server/source/services/server-socket/protocol808/functions/jt808ProcessPacket0x0102.ts
+++ dloc-server/source/services/server-socket/protocol808/functions/jt808ProcessPacket0x0102.ts
@@ -1,8 +1,10 @@
+import createHexFromNumberWithNBytes from "../../../../functions/createHexFromNumberWithNBytes";
 import { getNormalizedIMEI } from "../../../../functions/getNormalizedIMEI";
 import padNumberLeft from "../../../../functions/padNumberLeft";
 import { Jt808ProcessPacket } from "../models/Jt808ProcessPacket";
 import jt808CreateCheckParameterSettingPacket from "./jt808CreateCheckParameterSettingPacket";
 import jt808CreateGeneralResponse from "./jt808CreateGeneralResponse";
+import jt808CreateParameterSettingPacket from "./jt808CreateParameterSettingPacket";
 import jt808CreateQueryLocationMessage from "./jt808CreateQueryLocationMessage";
 import jt808PrintMessage from "./jt808PrintMessage";
 
@@ -23,6 +25,10 @@ const jt808ProcessPacket0x0102: Jt808ProcessPacket = async ({
   (response.response as Buffer[]).push(jt808CreateQueryLocationMessage(terminalId, counter + 100));
 
   (response.response as Buffer[]).push(jt808CreateCheckParameterSettingPacket(terminalId, counter + 102, []));
+
+  // (F102) Low battery alarm （0 off 1 on）default on
+  const parametersPackets = ["0000F102 04 " + createHexFromNumberWithNBytes(0, 2)];
+  (response.response as Buffer[]).push(jt808CreateParameterSettingPacket(terminalId, counter + 103, parametersPackets));
 
   response.imei = padNumberLeft(terminalId, 15, "0");
 
