a147516439d49345bf2e028d6bdca100e2bd4a91
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-18 20:51:54 +0200

    Fix interval report packet to use correct identifier for timing report

--- dloc-server/source/services/server-socket/protocol808/functions/jt808CreateIntervalReportPacket.ts
+++ dloc-server/source/services/server-socket/protocol808/functions/jt808CreateIntervalReportPacket.ts
@@ -9,7 +9,7 @@ const jt808CreateIntervalReportPacket = (
 ): Buffer => {
   /* Create interval report Packet */
   const intervalsPacket = jt808CreateParameterSettingPacket(terminalId, counter + 215, [
-    "00000000 04 " + createHexFromNumberWithNBytes(intervalSec, 4), // Report by timming
+    "00000020 04 " + createHexFromNumberWithNBytes(intervalSec, 4), // Report by timming
     "00000027 04 " + createHexFromNumberWithNBytes(intervalSec, 4), // Report time intervals during dormancy (seconds)
     "00000028 04 " + createHexFromNumberWithNBytes(intervalSec, 4), // Report time intervals during emergency alarm (seconds)
     "00000029 04 " + createHexFromNumberWithNBytes(intervalSec, 4), // Report time intervals when default (seconds)
