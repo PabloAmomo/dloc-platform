46d1b81c2018e3b2dada202c7ca963aed4117cbf
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-18 23:18:34 +0200

    Refactor parameter packets in jt808ProcessPacket0x0102 to include detailed GNSS positioning and upload mode settings

--- dloc-server/source/services/server-socket/protocol808/functions/jt808ProcessPacket0x0102.ts
+++ dloc-server/source/services/server-socket/protocol808/functions/jt808ProcessPacket0x0102.ts
@@ -25,7 +25,13 @@ const jt808ProcessPacket0x0102: Jt808ProcessPacket = async ({
   (response.response as Buffer[]).push(jt808CreateCheckParameterSettingPacket(terminalId, counter + 100, []));
 
   // (F102) Low battery alarm （0 off 1 on）default on
-  const parametersPackets = ["0000F102 01 " + createHexFromNumberWithNBytes(0, 2)];
+  // (0094) Upload mode of GNSS module detailed location data: 0x00, local storage, do not upload (default); 0x01, upload in time interval; 0x02, upload in distance interval; 0x0B, upload in accumulative time, automatically stop uploading after reaching transmission time; 0x0C, upload in accumulative distance, automatically stop uploading after reaching a certain distance 0x0D, upload in accumulative number of data, automatically stop uploading after reaching the number of uploads
+  // (0090) Definition of GNSS positioning mode is as follows: bit0, 0: disable GPS positioning, 1: enable GPS positioning,; bit1, 0: disable Beidou positioning, 1: enable Beidou positioning; bit2, 0: disable GLONASS positioning, 1: enable GLONASS positioning; bit3, 0: disable Galileo positioning, 1: enable Galileo positioning
+  const parametersPackets = [
+    "00000090 01 " + createHexFromNumberWithNBytes(255, 1),
+    "00000094 01 " + createHexFromNumberWithNBytes(1, 1),
+    "0000F102 01 " + createHexFromNumberWithNBytes(0, 1),
+  ];
   (response.response as Buffer[]).push(jt808CreateParameterSettingPacket(terminalId, counter + 101, parametersPackets));
 
   response.imei = padNumberLeft(terminalId, 15, "0");
