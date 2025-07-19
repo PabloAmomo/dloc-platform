79eceaf3e69e7f7ab559aef09786f06cdff14121
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-19 18:09:37 +0200

    Update interval report packet to correct report type and comment out unused fields; enhance GNSS positioning mode documentation

--- dloc-server/source/services/server-socket/protocol808/functions/jt808CreateIntervalReportPacket.ts
+++ dloc-server/source/services/server-socket/protocol808/functions/jt808CreateIntervalReportPacket.ts
@@ -9,17 +9,17 @@ const jt808CreateIntervalReportPacket = (
 ): Buffer => {
   /* Create interval report Packet */
   const intervalsPacket = jt808CreateParameterSettingPacket(terminalId, counter + 215, [
-    "00000020 04 " + createHexFromNumberWithNBytes(0, 4),           // report type (1 = timmming)
+    "00000020 01 " + createHexFromNumberWithNBytes(0, 1),           // report type (1 = timmming)
 
-    "00000027 04 " + createHexFromNumberWithNBytes(intervalSec, 4), // Report time intervals during dormancy (seconds)
-    "00000028 04 " + createHexFromNumberWithNBytes(intervalSec, 4), // Report time intervals during emergency alarm (seconds)
-    "00000029 04 " + createHexFromNumberWithNBytes(intervalSec, 4), // Report time intervals when default (seconds)
+    //"00000027 04 " + createHexFromNumberWithNBytes(intervalSec, 4), // Report time intervals during dormancy (seconds)
+    //"00000028 04 " + createHexFromNumberWithNBytes(intervalSec, 4), // Report time intervals during emergency alarm (seconds)
+    //"00000029 04 " + createHexFromNumberWithNBytes(intervalSec, 4), // Report time intervals when default (seconds)
 
     //"0000002C 04 " + createHexFromNumberWithNBytes(movementMeters, 4), // Report distance intervals when default moving (meters)
     //"0000002D 04 " + createHexFromNumberWithNBytes(movementMeters, 4), // Report distance intervals when moving during not login (meters)
     //"0000002E 04 " + createHexFromNumberWithNBytes(movementMeters, 4), // Report distance intervals when moving during dormancy (meters)
     //"0000002F 04 " + createHexFromNumberWithNBytes(movementMeters, 4), // Report distance intervals when moving during emergency (meters)
-    "00000031 04 " + createHexFromNumberWithNBytes(movementMeters, 4), // Geofence radius (meters)
+    //"00000031 04 " + createHexFromNumberWithNBytes(movementMeters, 4), // Geofence radius (meters)
   ]);
 
   return intervalsPacket;
--- dloc-server/source/services/server-socket/protocol808/functions/jt808ProcessPacket0x0102.ts
+++ dloc-server/source/services/server-socket/protocol808/functions/jt808ProcessPacket0x0102.ts
@@ -3,6 +3,7 @@ import { getNormalizedIMEI } from "../../../../functions/getNormalizedIMEI";
 import padNumberLeft from "../../../../functions/padNumberLeft";
 import { Jt808ProcessPacket } from "../models/Jt808ProcessPacket";
 import jt808CreateCheckParameterSettingPacket from "./jt808CreateCheckParameterSettingPacket";
+import jt808CreateFrameData from "./jt808CreateFrameData";
 import jt808CreateGeneralResponse from "./jt808CreateGeneralResponse";
 import jt808CreateParameterSettingPacket from "./jt808CreateParameterSettingPacket";
 import jt808PrintMessage from "./jt808PrintMessage";
@@ -25,13 +26,22 @@ const jt808ProcessPacket0x0102: Jt808ProcessPacket = async ({
 
   // (F102) Low battery alarm （0 off 1 on）default on
   // (0094) Upload mode of GNSS module detailed location data: 0x00, local storage, do not upload (default); 0x01, upload in time interval; 0x02, upload in distance interval; 0x0B, upload in accumulative time, automatically stop uploading after reaching transmission time; 0x0C, upload in accumulative distance, automatically stop uploading after reaching a certain distance 0x0D, upload in accumulative number of data, automatically stop uploading after reaching the number of uploads
-  // (0090) Definition of GNSS positioning mode is as follows: bit0, 0: disable GPS positioning, 1: enable GPS positioning,; bit1, 0: disable Beidou positioning, 1: enable Beidou positioning; bit2, 0: disable GLONASS positioning, 1: enable GLONASS positioning; bit3, 0: disable Galileo positioning, 1: enable Galileo positioning
+  // (0090) Definition of GNSS positioning mode is as follows: bit0, 0: disable GPS positioning, 1: enable GPS positioning | bit1, 0: disable Beidou positioning, 1: enable Beidou positioning | bit2, 0: disable GLONASS positioning, 1: enable GLONASS positioning | bit3, 0: disable Galileo positioning, 1: enable Galileo positioning
   const parametersPackets = [
     "00000090 01 " + createHexFromNumberWithNBytes(255, 1),
     "00000094 01 " + createHexFromNumberWithNBytes(1, 1),
     "0000F102 01 " + createHexFromNumberWithNBytes(0, 1),
   ];
   (response.response as Buffer[]).push(jt808CreateParameterSettingPacket(terminalId, counter + 101, parametersPackets));
+
+  (response.response as Buffer[]).push(
+    jt808CreateFrameData({
+      msgType: 0x8135,
+      terminalId: Buffer.from(terminalId, "hex"),
+      msgSerialNumber: counter + 102,
+      body: Buffer.from("09", "hex"),
+    })
+  );
 
   response.imei = padNumberLeft(terminalId, 15, "0");
 
