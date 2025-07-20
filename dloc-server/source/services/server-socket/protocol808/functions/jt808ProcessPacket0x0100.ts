e6c95fd49854542529be7986ee517e0cb8278e5b
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-20 21:57:10 +0200

    Update TODO comment for satellite constellation configuration to clarify origin and intent

--- dloc-server/source/services/server-socket/protocol808/functions/jt808ProcessPacket0x0100.ts
+++ dloc-server/source/services/server-socket/protocol808/functions/jt808ProcessPacket0x0100.ts
@@ -30,7 +30,7 @@ const jt808ProcessPacket0x0100: Jt808ProcessPacket = async ({
   // (0090) Definition of GNSS positioning mode is as follows: bit0, 0: disable GPS positioning, 1: enable GPS positioning | bit1, 0: disable Beidou positioning, 1: enable Beidou positioning | bit2, 0: disable GLONASS positioning, 1: enable GLONASS positioning | bit3, 0: disable Galileo positioning, 1: enable Galileo positioning
   // (0094) Upload mode of GNSS module detailed location data: 0x00, local storage, do not upload (default); 0x01, upload in time interval; 0x02, upload in distance interval; 0x0B, upload in accumulative time, automatically stop uploading after reaching transmission time; 0x0C, upload in accumulative distance, automatically stop uploading after reaching a certain distance 0x0D, upload in accumulative number of data, automatically stop uploading after reaching the number of uploads
   // (F102) Low battery alarm （0 off 1 on）default on
-  // TODO: [VERIFY] Configure sat constallation (Use all constellations)
+  // TODO: [VERIFY] (Moved from 0x102) NO Configure sat constallation (Use all constellations)
   const parametersPackets = [
     // "00000090 01 " + createHexFromNumberWithNBytes(255, 1), // -> GPS, Beidu, and Galileo positioning 00001011 = 11 (Bit 0, 1 y Bit 3 )
     "00000094 01 " + createHexFromNumberWithNBytes(1, 1),
