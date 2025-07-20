8d14b1836f43dad09c395774e82709bb0e4b149c
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-20 20:35:18 +0200

    Update GPS constellation configuration to enable Beidou positioning and improve comments for clarity

--- dloc-server/source/services/server-socket/protocol808/functions/jt808ProcessPacket0x0102.ts
+++ dloc-server/source/services/server-socket/protocol808/functions/jt808ProcessPacket0x0102.ts
@@ -29,8 +29,9 @@ const jt808ProcessPacket0x0102: Jt808ProcessPacket = async ({
   // (F102) Low battery alarm （0 off 1 on）default on
   // (0094) Upload mode of GNSS module detailed location data: 0x00, local storage, do not upload (default); 0x01, upload in time interval; 0x02, upload in distance interval; 0x0B, upload in accumulative time, automatically stop uploading after reaching transmission time; 0x0C, upload in accumulative distance, automatically stop uploading after reaching a certain distance 0x0D, upload in accumulative number of data, automatically stop uploading after reaching the number of uploads
   // (0090) Definition of GNSS positioning mode is as follows: bit0, 0: disable GPS positioning, 1: enable GPS positioning | bit1, 0: disable Beidou positioning, 1: enable Beidou positioning | bit2, 0: disable GLONASS positioning, 1: enable GLONASS positioning | bit3, 0: disable Galileo positioning, 1: enable Galileo positioning
+  // TODO: [VERIFY] Check GPS constallation configuration (GPS, Beidou, Galileo)
   const parametersPackets = [
-    "00000090 01 " + createHexFromNumberWithNBytes(9, 1), // Enable GPS and Galileo positioning (Bit 0 y Bit 3)
+    "00000090 01 " + createHexFromNumberWithNBytes(11, 1), // Enable GPS, Beidu, and Galileo positioning (Bit 0, 1 y Bit 3)
     "00000094 01 " + createHexFromNumberWithNBytes(1, 1),
     "0000F102 01 " + createHexFromNumberWithNBytes(0, 1),
   ];
