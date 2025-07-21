e786b1ea4db47e10ec2ab0aed2313c613b2bea42
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-21 08:52:54 +0200

    Update REPORT_CONFIGURATION in jt808Config to use temporaryTracking for improved tracking accuracy

--- dloc-server/source/services/server-socket/protocol808/config/jt808Config.ts
+++ dloc-server/source/services/server-socket/protocol808/config/jt808Config.ts
@@ -1,7 +1,7 @@
 import Jt808ReportConfiguration from "../enums/Jt808reportConfiguration";
 
 const jt808Config: { REPORT_CONFIGURATION: Jt808ReportConfiguration;  } = {
-  REPORT_CONFIGURATION: Jt808ReportConfiguration.hybridRport,
+  REPORT_CONFIGURATION: Jt808ReportConfiguration.temporaryTracking,
 };
 
 export default jt808Config;
