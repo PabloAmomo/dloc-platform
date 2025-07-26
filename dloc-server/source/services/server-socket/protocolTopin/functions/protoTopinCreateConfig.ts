684f8d9ce0823343a812174617f25a112c22be89
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-26 14:09:57 +0200

    Add additional packets for uploadSec in protoTopinCreateConfig function

--- dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateConfig.ts
+++ dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateConfig.ts
@@ -13,6 +13,10 @@ function protoTopinCreateConfig(powerProfileType: PowerProfileType): Buffer[] {
   response.push(protoTopinCreatePacket(Buffer.from([0x05, 0x97, 0x00, uploadSec])));
   response.push(protoTopinCreatePacket(Buffer.from([0x06, 0x97, 0x00, uploadSec])));
 
+  response.push(protoTopinCreatePacket(Buffer.from([0x03, 0x98, 0x00, uploadSec])));
+  response.push(protoTopinCreatePacket(Buffer.from([0x04, 0x98, 0x00, uploadSec])));
+  response.push(protoTopinCreatePacket(Buffer.from([0x05, 0x98, 0x00, uploadSec])));
+  response.push(protoTopinCreatePacket(Buffer.from([0x06, 0x98, 0x00, uploadSec])));
   // TODO: Add packet to configure heartbeat interval
 
   return response;
