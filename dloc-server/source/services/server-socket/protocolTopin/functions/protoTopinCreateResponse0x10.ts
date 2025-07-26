5d1aa78aefcc64ae0d890f6890ba526005a1a099
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-26 07:52:05 +0200

    Fix response creation in protoTopinCreateResponse0x10 by updating packet header

--- dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateResponse0x10.ts
+++ dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateResponse0x10.ts
@@ -2,8 +2,10 @@ import { ProtoTopinPacket } from "../models/ProtoTopinPacket";
 import protoTopinCreatePacket from "./protoTopinCreatePacket";
 
 // TODO: Unify the responses when its posible
+
+// TODO: Unify with protoTopinProcessPacket0x18
 const protoTopinCreateResponse0x10 = (gt06Packer: ProtoTopinPacket, datetime: Buffer): Buffer => {
-  return protoTopinCreatePacket(Buffer.from([0x00, 0x10, ...datetime]));
+  return protoTopinCreatePacket(Buffer.from([0x08, 0x10, ...datetime]));
 }
 
 export default protoTopinCreateResponse0x10;
