89ee4d93a6ff9ce0cd9c519f5bd7bc7b4b165442
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-26 08:33:18 +0200

    Fix response identifier in protoTopinCreateResponse0x18 for correct packet creation

--- dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateResponse0x18.ts
+++ dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateResponse0x18.ts
@@ -5,7 +5,7 @@ import protoTopinCreatePacket from "./protoTopinCreatePacket";
 
 // TODO: Unify with protoTopinProcessPacket0x10
 const protoTopinCreateResponse0x18 = (TopinPacket: ProtoTopinPacket, datetime: Buffer): Buffer => {
-  return protoTopinCreatePacket(Buffer.from([0x08, 0x10, ...datetime]));
+  return protoTopinCreatePacket(Buffer.from([0x08, 0x18, ...datetime]));
 }
 
 export default protoTopinCreateResponse0x18;
