b5ad36ceb7bf49faa689c8ff68358f7a8fa6ca2c
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-26 22:14:15 +0200

    Remove commented-out response creation for protoTopinCreateResponse0x97 and protoTopinCreateResponse0x98 in protoTopinCreateConfig

--- dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateConfig.ts
+++ dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateConfig.ts
@@ -8,8 +8,9 @@ function protoTopinCreateConfig(powerProfileType: PowerProfileType): Buffer[] {
   let response: Buffer[] = [];
   const { heartBeatSec, uploadSec, ledState } = protoTopinPowerProfileConfig(powerProfileType);
 
-  response.push(protoTopinCreateResponse0x97(uploadSec));
-  response.push(protoTopinCreateResponse0x98(uploadSec));
+  // TODO: [VERIFY] Not needed for now
+  //response.push(protoTopinCreateResponse0x97(uploadSec));
+  //response.push(protoTopinCreateResponse0x98(uploadSec));
 
   let uploadIntervalMin = Math.floor(uploadSec / 60); 
   if (uploadIntervalMin === 0) uploadIntervalMin = 1; // Setting a default value of 1 minute if undefined
