584f4e2a1fef1dd65616d8eff2532907477c0717
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-26 21:33:02 +0200

    Rename parameter from 'interval' to 'intervalSec' in protoTopinCreateResponse functions for clarity

--- dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreatePacket0x97.ts
+++ dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreatePacket0x97.ts
@@ -1,7 +1,7 @@
 import protoTopinCreatePacket from "./protoTopinCreatePacket";
 
-function protoTopinCreateResponse0x97(interval: number): Buffer {
-  return protoTopinCreatePacket(Buffer.from([0x03, 0x97, 0x00, interval])); 
+function protoTopinCreateResponse0x97(intervalSec: number): Buffer {
+  return protoTopinCreatePacket(Buffer.from([0x03, 0x97, 0x00, intervalSec])); 
 }
 
 export default protoTopinCreateResponse0x97;
