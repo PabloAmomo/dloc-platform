2831acdfd0b3cc042cb832c649801700877d26e9
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-26 08:17:06 +0200

    Update response lengths for protoTopinCreateResponse0x13, protoTopinCreateResponse0x30, and protoTopinCreateResponse0xB3 for protocol consistency

--- dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateResponse0x13.ts
+++ dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateResponse0x13.ts
@@ -3,7 +3,7 @@ import protoTopinCreatePacket from "./protoTopinCreatePacket";
 
 function protoTopinCreateResponse0x13(TopinPacket: ProtoTopinPacket, intervalTimeMin: number): Buffer {
   return protoTopinCreatePacket(Buffer.from([
-    0x02, // Length
+    0x03, // Length
     0x13, // Protocol number
     intervalTimeMin, // Interval time in minutes
   ])); 
--- dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateResponse0x30.ts
+++ dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateResponse0x30.ts
@@ -6,7 +6,7 @@ function protoTopinCreateResponse0x30(TopinPacket: ProtoTopinPacket): Buffer {
   const { year, month, day, hours, minutes, seconds  } = getDateTimeValues(new Date());
 
   const responsePayload = Buffer.from([
-    0x07,
+    0x08,
     0x30, 
     year, // Year
     month, // Month
