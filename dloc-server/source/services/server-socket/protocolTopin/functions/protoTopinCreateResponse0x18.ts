d64e4cc4bc9247b470e5fe53c8df4970e947efd5
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-26 07:52:11 +0200

    Add protoTopinCreateResponse0x18 function for response creation

---
+++ dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateResponse0x18.ts
@@ -0,0 +1,11 @@
+import { ProtoTopinPacket } from "../models/ProtoTopinPacket";
+import protoTopinCreatePacket from "./protoTopinCreatePacket";
+
+// TODO: Unify the responses when its posible
+
+// TODO: Unify with protoTopinProcessPacket0x10
+const protoTopinCreateResponse0x18 = (gt06Packer: ProtoTopinPacket, datetime: Buffer): Buffer => {
+  return protoTopinCreatePacket(Buffer.from([0x08, 0x10, ...datetime]));
+}
+
+export default protoTopinCreateResponse0x18;
