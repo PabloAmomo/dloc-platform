10b92f13a3351248d0b77aad9a764701cec12777
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-26 14:05:42 +0200

    Add packet creation for uploadSec in protoTopinCreateConfig function

--- dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateConfig.ts
+++ dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateConfig.ts
@@ -1,4 +1,5 @@
 import { PowerProfileType } from "../../../../enums/PowerProfileType";
+import protoTopinCreatePacket from "./protoTopinCreatePacket";
 import protoTopinCreateResponse0x97 from "./protoTopinCreatePacket0x97";
 import protoTopinPowerProfileConfig from "./protoTopinGetPowerProfileConfig";
 
@@ -7,6 +8,10 @@ function protoTopinCreateConfig(powerProfileType: PowerProfileType): Buffer[] {
   const { heartBeatSec, uploadSec, ledState } = protoTopinPowerProfileConfig(powerProfileType);
 
   response.push(protoTopinCreateResponse0x97(uploadSec)); // Start of packet
+
+  response.push(protoTopinCreatePacket(Buffer.from([0x04, 0x97, 0x00, uploadSec])));
+  response.push(protoTopinCreatePacket(Buffer.from([0x05, 0x97, 0x00, uploadSec])));
+  response.push(protoTopinCreatePacket(Buffer.from([0x06, 0x97, 0x00, uploadSec])));
 
   // TODO: Add packet to configure heartbeat interval
 
