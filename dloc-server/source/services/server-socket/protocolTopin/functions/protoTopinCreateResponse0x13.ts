1d65146efefd3f549d007500d39458936ff0966d
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-29 22:26:50 +0200

    Refactor protoTopinCreatePacket0x13 and protoTopinCreateResponse0x13 for improved clarity and handling of zero intervals

--- dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateResponse0x13.ts
+++ dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateResponse0x13.ts
@@ -1,9 +1,13 @@
 import protoTopinCreatePacket from "./protoTopinCreatePacket";
 
 function protoTopinCreateResponse0x13(intervalTimeMin: number, heartBeatIntervalSec: number): Buffer[] {
-  const packetUploadInterval = protoTopinCreatePacket(Buffer.from([0x02, 0x13, intervalTimeMin]));
+  const packetUploadInterval =
+    intervalTimeMin !== 0 ? protoTopinCreatePacket(Buffer.from([0x02, 0x13, intervalTimeMin])) : Buffer.alloc(0);
 
-  const heartBeatInterval = protoTopinCreatePacket(Buffer.from([0x03, 0x13, 0x00, heartBeatIntervalSec]));
+  const heartBeatInterval =
+    heartBeatIntervalSec !== 0
+      ? protoTopinCreatePacket(Buffer.from([0x03, 0x13, 0x00, heartBeatIntervalSec]))
+      : Buffer.alloc(0);
 
   return [packetUploadInterval, heartBeatInterval];
 }
