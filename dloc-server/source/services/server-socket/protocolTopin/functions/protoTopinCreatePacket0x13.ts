a353cad1ceed445af626077b243532a2ac8363c5
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-29 22:51:42 +0200

    Fix log messages in protoTopinCreatePacket0x13 to clarify setting intervals instead of requesting them

--- dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreatePacket0x13.ts
+++ dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreatePacket0x13.ts
@@ -8,8 +8,8 @@ const protoTopinCreatePacket0x13 = (prefix: string, powerProfileType: PowerProfi
   let heartBeatIntervalMin = Math.floor(heartBeatSec / 60);
   if (heartBeatIntervalMin === 0) heartBeatIntervalMin = 1;
 
-  printMessage(`${prefix} 🆙 Request hearbeat interval to ${heartBeatIntervalMin} minutes.`);
-  printMessage(`${prefix} ❤️  Request upload interval to ${uploadSec} seconds.`);
+  printMessage(`${prefix} 🆙 Setting hearbeat interval to ${heartBeatIntervalMin} minutes.`);
+  printMessage(`${prefix} ❤️  Setting upload interval to ${uploadSec} seconds.`);
 
   // DEBUG: Only send heartbeat update /Using 0x97 para el updaloadSec
   return protoTopinCreateResponse0x13(heartBeatIntervalMin, uploadSec);
