60426ac2ee388fafad55282d01b30906e2018a51
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-29 23:33:39 +0200

    Refactor protoTopinCreateResponse0x13 to improve parameter naming and clarify packet creation logic; restore upload interval packet handling

--- dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateResponse0x13.ts
+++ dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateResponse0x13.ts
@@ -1,18 +1,23 @@
 import protoTopinCreatePacket from "./protoTopinCreatePacket";
 import protoTopinCreatePacket0x44 from "./protoTopinCreatePacket0x44";
+import protoTopinCreatePacket0x97 from "./protoTopinCreatePacket0x97";
 
-function protoTopinCreateResponse0x13(intervalTimeMin: number, heartBeatIntervalSec: number): Buffer[] {
+function protoTopinCreateResponse0x13(heartBeatIntervalMin: number, uploadIntervalSec: number): Buffer[] {
   const packetStopUpload = protoTopinCreatePacket0x44();
 
-  const packetUploadInterval =
-    intervalTimeMin !== 0 ? protoTopinCreatePacket(Buffer.from([0x02, 0x13, intervalTimeMin])) : Buffer.alloc(0);
-
-  const heartBeatInterval =
-    heartBeatIntervalSec !== 0
-      ? protoTopinCreatePacket(Buffer.from([0x03, 0x13, 0x00, heartBeatIntervalSec]))
+  const packetHeartBeatIntervalMin =
+    heartBeatIntervalMin !== 0
+      ? protoTopinCreatePacket(Buffer.from([0x02, 0x13, heartBeatIntervalMin]))
       : Buffer.alloc(0);
 
-  return [packetStopUpload, packetUploadInterval, heartBeatInterval];
+  //const packetUploadIntervalSec =
+  //  uploadIntervalSec !== 0
+  //    ? protoTopinCreatePacket(Buffer.from([0x03, 0x13, 0x00, uploadIntervalSec]))
+  //    : Buffer.alloc(0);
+
+  const packetUploadIntervalSec = protoTopinCreatePacket0x97(uploadIntervalSec);
+
+  return [packetStopUpload, packetHeartBeatIntervalMin, packetUploadIntervalSec];
 }
 
 export default protoTopinCreateResponse0x13;
