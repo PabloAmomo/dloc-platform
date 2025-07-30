9d6b36279ece881f09f64639922440a9e38589e3
Pablo Amommo <pablo.amomo@gmail.com>
2025-07-30 10:24:33 +0200

    Add logging for input lists in countMatchingMacAddresses function for debugging purposes

--- dloc-server/source/functions/countMatchingMacAddresses.ts
+++ dloc-server/source/functions/countMatchingMacAddresses.ts
@@ -3,6 +3,9 @@ import { WifiAccessPoint } from "../models/WifiAccessPoint";
 const countMatchingMacAddresses = (list1: WifiAccessPoint[], list2: WifiAccessPoint[]): number  => {
   const macSet = new Set(list1.map((device) => device.macAddress.toLowerCase()));
 
+  console.log("---> (1)", JSON.stringify(list1));
+  console.log("---> (2)", JSON.stringify(list2));
+  
   let count = 0;
   for (const device of list2) {
     if (macSet.has(device.macAddress.toLowerCase())) count++;
