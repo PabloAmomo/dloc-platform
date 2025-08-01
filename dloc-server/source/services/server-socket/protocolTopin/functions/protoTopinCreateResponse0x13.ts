230f849115819b8afeddccf2329f4f04c521cc8c
Pablo Amommo <pablo.amomo@gmail.com>
2025-08-01 15:01:22 +0200

    feat: Add additional packet creation for heartBeatIntervalMin in protoTopinCreateResponse0x13 function

--- dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateResponse0x13.ts
+++ dloc-server/source/services/server-socket/protocolTopin/functions/protoTopinCreateResponse0x13.ts
@@ -4,6 +4,8 @@ function protoTopinCreateResponse0x13(heartBeatIntervalMin: number, uploadInterv
   const packets = [];
 
   if (heartBeatIntervalMin !== 0) packets.push(protoTopinCreatePacket(Buffer.from([0x02, 0x13, heartBeatIntervalMin])));
+  // TODO: Testing if get 0x13 value
+  if (heartBeatIntervalMin !== 0) packets.push(protoTopinCreatePacket(Buffer.from([0x01, 0x13, heartBeatIntervalMin])));
 
   if (uploadIntervalSec !== 0) packets.push(protoTopinCreatePacket(Buffer.from([0x03, 0x13, 0x00, uploadIntervalSec])));
 
