2a632596eed4bd0276cc1f7b1592da9f626020a4
Pablo Amommo <pablo.amomo@gmail.com>
2025-08-12 09:46:39 +0200

    feat: Implement handleGetLastPosition function for retrieving device location data

---
+++ dloc-server/source/infraestucture/mySql/handles/handleGetLastPosition.ts
@@ -0,0 +1,15 @@
+import { ConnectionConfig } from 'mysql';
+import { mySqlConnectionConfig } from '../functions/mySqlConnectionConfig';
+import { PersistenceResult } from '../../models/PersistenceResult';
+import mySqlQueryAsync from '../functions/mySqlQueryAsync';
+
+const connectionConfig: ConnectionConfig = mySqlConnectionConfig;
+
+const handleGetLastPosition = async (imei: string): Promise<PersistenceResult> => {
+  const params = [imei];
+  const sql = `SELECT lat, lng, lastVisibilityUTC FROM device WHERE imei = ?;`;
+  const response: PersistenceResult = await mySqlQueryAsync(connectionConfig, sql, params);
+  return response;
+};
+
+export { handleGetLastPosition };
