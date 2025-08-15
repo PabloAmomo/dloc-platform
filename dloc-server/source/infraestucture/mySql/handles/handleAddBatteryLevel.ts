bb6fc4f83209d1ef0cbf4321b4d289bd2f7d1df7
Pablo Amommo <pablo.amomo@gmail.com>
2025-08-15 07:24:58 +0200

    refactor: Simplify SQL query formatting in handleAddBatteryLevel function

--- dloc-server/source/infraestucture/mySql/handles/handleAddBatteryLevel.ts
+++ dloc-server/source/infraestucture/mySql/handles/handleAddBatteryLevel.ts
@@ -6,33 +6,24 @@ import mySqlQueryAsync from "../functions/mySqlQueryAsync";
 
 const connectionConfig: ConnectionConfig = mySqlConnectionConfig;
 
-const handleAddBatteryLevel = async (
-  imei: string,
-  batteryLevel: number
-): Promise<PersistenceResult> => {
+const handleAddBatteryLevel = async (imei: string, batteryLevel: number): Promise<PersistenceResult> => {
   const updateBattery = batteryLevel !== -1;
   const params = updateBattery ? [imei, batteryLevel, batteryLevel] : [imei];
 
   const sql = updateBattery
     ? `
-      INSERT INTO device (imei, batteryLevel, lastVisibilityUTC)
-      VALUES (?, ?, UTC_TIMESTAMP())
+      INSERT INTO device (imei, batteryLevel, lastVisibilityUTC) VALUES (?, ?, UTC_TIMESTAMP())
       ON DUPLICATE KEY UPDATE 
         batteryLevel = ?, 
         lastVisibilityUTC = UTC_TIMESTAMP();
     `
     : `
-      INSERT INTO device (imei, lastVisibilityUTC)
-      VALUES (?, UTC_TIMESTAMP())
+      INSERT INTO device (imei, lastVisibilityUTC) VALUES (?, UTC_TIMESTAMP())
       ON DUPLICATE KEY UPDATE 
         lastVisibilityUTC = UTC_TIMESTAMP();
     `;
 
-  const response: PersistenceResult = await mySqlQueryAsync(
-    connectionConfig,
-    sql,
-    params
-  );
+  const response: PersistenceResult = await mySqlQueryAsync(connectionConfig, sql, params);
   if (!response?.error) await mySqlClonedImeiUpdate(connectionConfig, imei);
   return response;
 };
